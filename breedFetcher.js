2fb917f0280f0472a3ca666b4ead589f87872e08
Frederick <frederick.gfl@gmail.com>
2019-12-06 00:33:54 +0000

    Update test file to match refactored code

--- breedFetcher.js
+++ breedFetcher.js
@@ -7,27 +7,27 @@
 
 const request = require('request');
 
+
 const CAT_NOT_FOUND_MSG = cat => `"${cat}" was not found.`;
 
 /**
  * get the description of the cat using theCatAPI
  * @param {string} breedName: name of cat
  * @param {function} callback (error, description)
- *    error - null if successful
- *    description - null if failure
+ *    error - null if successful. Something if failure.
+ *    description - string if successful. null if failure
  */
 const fetchBreedDescription = (breedName, callback) => {
   const theCatApiUrl = 'https://api.thecatapi.com/v1/breeds/';
   request(`${theCatApiUrl}search?q=${breedName}`, (err, response, body) => {
     let result = '';
 
-    if (err) {
-      result = 'There was an error getting the information. Error: ', err.code;
-    } else {
+    if (!err) {
       const data = JSON.parse(body)[0];
 
       if (!data) {
-        result = CAT_NOT_FOUND_MSG(breedName);
+        result = null;
+        err = new Error(`${CAT_NOT_FOUND_MSG(breedName)}`);
       } else {
         result = data.description;
       }
--- test/breedFetcherTest.js
+++ test/breedFetcherTest.js
@@ -5,42 +5,31 @@
 
 const assert = require('chai').assert;
 
-const request = require('request');
-const {
-  handleCatDbSearch,
-  CAT_NOT_FOUND_MSG,
-} = require('../handleCatDbSearch');
+const {fetchBreedDescription} = require('../breedFetcher');
+// const {
+//   handleCatDbSearch,
+//   CAT_NOT_FOUND_MSG,
+// } = require('../handleCatDbSearch');
 
 
-describe("#handleCatDbSearch() callback function", () => {
-  const theCatApiUrl = 'https://api.thecatapi.com/v1/breeds/';
-  const badUrl = 'https://api.theatapi.com/notaversion/breeds';
+describe("#fetchBreedDescription() callback function", () => {
 
-  it("should return description for the cat when called with valid cat", (done) => {
+  it("returns a string description for a valid breed, via callback", (done) => {
     const catToLookUp = 'Chartreux';
     const expected = 'The Chartreux is generally silent but communicative. Short play sessions, mixed with naps and meals are their perfect day. Whilst appreciating any attention you give them, they are not demanding, content instead to follow you around devotedly, sleep on your bed and snuggle with you if you’re not feeling well.';
 
-    request(`${theCatApiUrl}search?q=${catToLookUp}`, (err, resp, body) => {
-      const description = handleCatDbSearch(err, resp, body);
-      assert.equal(description, expected);
+    fetchBreedDescription(catToLookUp, (err, desc) => {
+      assert.equal(err, null);
+      assert.equal(expected, desc);
       done();
     });
   });
-  it("should return not found message when called with invalid cat", (done) => {
+  it("should return null when called with invalid cat", (done) => {
     const catToLookUp = 'no cat';
-    const expected = CAT_NOT_FOUND_MSG(catToLookUp);
-    request(`${theCatApiUrl}search?q=${catToLookUp}`, (err, resp, body) => {
-      const description = handleCatDbSearch(err, resp, body, catToLookUp);
-      assert.equal(description, expected);
-      done();
-    });
-  });
-  it("should return undefined when called with invalid URL", (done) => {
-    const catToLookUp = 'Chartreux';
-    const expected = undefined;
-    request(`${badUrl}search?q=${catToLookUp}`, (err, resp, body) => {
-      const description = handleCatDbSearch(err, resp, body, catToLookUp);
-      assert.equal(description, expected);
+    const expected = null;
+    fetchBreedDescription(catToLookUp, (err, desc) => {
+      assert.notEqual(err, null);
+      assert.equal(expected, desc);
       done();
     });
   });
